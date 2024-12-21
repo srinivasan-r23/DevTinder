3c43f6a055f83423ebce37547ff143f924630da0
Srini <srinismart2@gmail.com>
2024-12-21 20:41:36 +0530

    delete, find, feed, user update

--- src/app.js
+++ src/app.js
@@ -15,6 +15,54 @@ app.post("/signup", async (req, res) => {
   }
 });
 
+app.get("/user", async (req, res) => {
+  const email = req?.query?.email;
+  try {
+    const user = await userModel.findOne({ email });
+    console.log(user);
+    if (user?.id) res.send(user);
+    else res.status(404).send({ message: "User not found" });
+  } catch (err) {
+    res.status(500).send({ message: err?.message });
+  }
+});
+
+app.delete("/user", async (req, res) => {
+  const id = req?.query?.id;
+  try {
+    const user = await userModel.findByIdAndDelete(id);
+    if (user) res.send({ message: "User deleted successfully" });
+    else res.status(404).send({ message: "User not found" });
+  } catch (err) {
+    res.status(500).send({ message: err?.message });
+  }
+});
+
+app.get("/feed", async (req, res) => {
+  try {
+    const users = await userModel.find({});
+    res.send(users);
+  } catch (err) {
+    res.status(500).send({ message: err.message });
+  }
+});
+
+app.patch("/user", async (req, res) => {
+  const data = req.body;
+  try {
+    const doc = await userModel.findByIdAndUpdate(data?.id, req?.body, {
+      returnDocument: "after",
+    });
+    if (doc) {
+      res.send({ message: "User updated successfully" });
+    } else {
+      res.status(404).send({ message: "User not found" });
+    }
+  } catch (err) {
+    res.status(500).send({ message: err.message });
+  }
+});
+
 connectDB()
   .then(() => {
     console.log("DB connection established");
